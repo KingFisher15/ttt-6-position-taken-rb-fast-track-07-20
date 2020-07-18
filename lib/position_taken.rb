2518e4cd5bd00e4e51e48ef35701733016be5e65
crisp-delimiter-5393 <gordond227@gmail.com>
2020-07-18 18:36:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
 def position_taken?(board, position)
 
   (board[position] ==  " " || board[position] == "" || board[position] == nil) ?
-  
+
   (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
     false : true
 
